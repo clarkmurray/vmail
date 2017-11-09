b67fc6845789333aaa96b306a1aad5a193927342
Clark Murray <clarkamurray@gmail.com>
2017-11-09 14:43:18 -0500

    Can now respond to messages

--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -22,6 +22,34 @@
 
 				</div>
 			</div>
+
+             <div class="panel panel-default">
+                <div class="panel-heading"><h2>Reply</h2></div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="/messages">
+                        {{ csrf_field() }}
+                        
+                        <input name="recipient" type="hidden" value="{{ $message->sender->name }}">
+                        <input name="subject" type="hidden" value="{{ $message->subject }}">
+                        <div class="form-group">
+                            <label for="messageContent">Message:</label>
+                            <textarea class="form-control" id="messageContent" name="messageContent" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Send</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+
 		</div>
 	</div>
 </div>
