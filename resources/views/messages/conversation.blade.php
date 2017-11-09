277db1b71ccce41f292def48c0a5874aa33a4cfa
Clark Murray <clarkamurray@gmail.com>
2017-11-09 16:59:28 -0500

    Messages in panels

--- resources/views/messages/conversation.blade.php
+++ resources/views/messages/conversation.blade.php
@@ -2,13 +2,25 @@
 
 @section('content')
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Conversation</div>
+
 	@foreach ($conversation as $message)
-		<div>
-			<li>{{ $message->sender->name }}</li>
-			<li>{{ $message->created_at }}</li>
-			<li>{{ $message->body }}</li>
+	<div class="panel panel-default">
+        <div class="panel-heading">{{ $message->sender->name }}</div>
+        <div class="panel-body">
+			{{ $message->body }}
 		</div>
+	</div>
 	@endforeach
+
+</div>
+</div>
+</div>
+</div>
 	
 
 @endsection
